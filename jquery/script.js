68382cf9e10e54ba4e05803ccbb753490b7a9882
Solo Solly <githua49@gmail.com>
2020-02-21 14:36:46 +0300

    add toggle effect

---
+++ jquery/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".oneclick").click (function(){
+        $("#designimage").toggle();
+        $("#indesign").toggle();
+    });
+
+    $(".onclick").click (function(){
+        $("#devclick").toggle();
+        $("#indevelopment").toggle();
+    });
+
+    $(".onclickk").click (function(){
+        $("#productclick").toggle();
+        $("#inproduct").toggle();
+    });
+
+
+    
+});
+
+    
+   
