bc19e82feda59233bdfc68956df2eb3e58b13e01
Solo Solly <githua49@gmail.com>
2020-02-24 00:26:47 +0300

    add form validation

--- jquery/script.js
+++ jquery/script.js
@@ -14,10 +14,21 @@ $(document).ready(function(){
         $("#inproduct").toggle();
     });
 
+    $("#submit").click(function(){
+		var name = $("#Name").val();
+		var email = $("#Email").val();
+		var info = $("#Message").val();
+		$(".Name").text(name);
+		$(".Email").text(email);
+		$(".Message").text(info);
+        $("#output").show();
+        alert("Thank you " + name + " for your feedback. ");
+        // alert("Thank you " + name + " for your feedback. " +email);
+        
+        
+    });
+
 
 
     
 });
-
-    
-   
